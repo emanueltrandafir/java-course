b73a638324bd473f8c6ad7c19f0bd8560bf7ffe3
ovtep <ovidiu.tepescu@gmail.com>
2020-02-14 15:04:22 +0200

    resolve my impl

--- src/service/MemoryStorageImpl.java
+++ src/service/MemoryStorageImpl.java
@@ -3,40 +3,53 @@ package service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import model.Item; 
 
 public class MemoryStorageImpl implements ServiceInterface{
 
+	Map<Integer, Item> itemMap  = new HashMap<>();
+	int counter = 0;
+	
 	@Override
 	public int createItem(Item item) {
-		// TODO Auto-generated method stub
-		return 0;
+		counter++;
+		item.setId(counter);
+		itemMap.put(item.getId(), item);
+		return counter;
 	}
 
 	@Override
 	public Item readItem(int id) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return null;
+		if(!itemMap.containsKey(id)) {
+			throw new NullPointerException();
+		}
+		return itemMap.get(id);
 	}
 
 	@Override
 	public void updateItem(int id, Item item) throws NullPointerException {
-		// TODO Auto-generated method stub
+		if(!itemMap.containsKey(id)) {
+			throw new NullPointerException();
+		}
+		item.setId(id);
+		itemMap.put(id, item);
 		
 	}
 
 	@Override
 	public void deleteItem(int id) throws NullPointerException {
-		// TODO Auto-generated method stub
-		
+		if(!itemMap.containsKey(id)) {
+			throw new NullPointerException();
+		}
+		itemMap.remove(id);
 	}
 
 	@Override
 	public List<Item> readAllItems() {
-		// TODO Auto-generated method stub
-		return null;
+		return itemMap.values().stream().collect(Collectors.toList());
 	}
  
 	
